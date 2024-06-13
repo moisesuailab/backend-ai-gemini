de452fbf524bbf260f9ecfa66a2ec4530c42b412
moisesuailab <157806024+moisesuailab@users.noreply.github.com>
2024-06-13 16:29:14 +0000

    refact: puppeteer

--- build/puppeteer.config.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const puppeteerConfig = {
-    cacheDirectory: (0, path_1.join)(__dirname, '.cache', 'puppeteer'),
-};
-exports.default = puppeteerConfig;
